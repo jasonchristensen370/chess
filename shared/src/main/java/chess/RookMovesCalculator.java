f6ea18d4205227961b75e93512723e5e1a3e4346
jasonchristensen370 <jasonchristensen370@gmail.com>
2025-01-17 20:16:43 -0700

    Implemented pieceMoves for QueenMovesCalculator and RookMovesCalculator

--- shared/src/main/java/chess/RookMovesCalculator.java
+++ shared/src/main/java/chess/RookMovesCalculator.java
@@ -1,4 +1,89 @@
 package chess;
 
-public class RookMovesCalculator  implements PieceMovesCalculator{
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class RookMovesCalculator  implements PieceMovesCalculator {
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
+
+        Collection<ChessMove> validMoves = new ArrayList<>();
+        int max_moves = 7;
+        int row = position.getRow();
+        int col = position.getColumn();
+        ChessGame.TeamColor my_color = board.getPiece(position).getTeamColor();
+
+        /* Check Moves to the Left */
+        for (int i=1; i <= max_moves; i++) {
+            ChessPosition test_move = new ChessPosition(row, col-i);
+            int test_row = test_move.getRow();
+            int test_col = test_move.getColumn();
+            if (test_row <= 8 && test_col <= 8 && test_row >= 1 && test_col >= 1) {
+                if (board.getPiece(test_move) == null) {
+                    validMoves.add(new ChessMove(position, test_move, null));
+                } else if (board.getPiece(test_move).getTeamColor() != my_color) {
+                    validMoves.add(new ChessMove(position, test_move, null));
+                    break;
+                } else { /* Is my_color. Don't add and stop in this direction */
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+        /* Check Moves to the Right */
+        for (int i=1; i <= max_moves; i++) {
+            ChessPosition test_move = new ChessPosition(row, col+i);
+            int test_row = test_move.getRow();
+            int test_col = test_move.getColumn();
+            if (test_row <= 8 && test_col <= 8 && test_row >= 1 && test_col >= 1) {
+                if (board.getPiece(test_move) == null) {
+                    validMoves.add(new ChessMove(position, test_move, null));
+                } else if (board.getPiece(test_move).getTeamColor() != my_color) {
+                    validMoves.add(new ChessMove(position, test_move, null));
+                    break;
+                } else { /* Is my_color. Don't add and stop in this direction */
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+        /* Check Moves Up */
+        for (int i=1; i <= max_moves; i++) {
+            ChessPosition test_move = new ChessPosition(row+i, col);
+            int test_row = test_move.getRow();
+            int test_col = test_move.getColumn();
+            if (test_row <= 8 && test_col <= 8 && test_row >= 1 && test_col >= 1) {
+                if (board.getPiece(test_move) == null) {
+                    validMoves.add(new ChessMove(position, test_move, null));
+                } else if (board.getPiece(test_move).getTeamColor() != my_color) {
+                    validMoves.add(new ChessMove(position, test_move, null));
+                    break;
+                } else { /* Is my_color. Don't add and stop in this direction */
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+        /* Check Moves Down */
+        for (int i=1; i <= max_moves; i++) {
+            ChessPosition test_move = new ChessPosition(row-i, col);
+            int test_row = test_move.getRow();
+            int test_col = test_move.getColumn();
+            if (test_row <= 8 && test_col <= 8 && test_row >= 1 && test_col >= 1) {
+                if (board.getPiece(test_move) == null) {
+                    validMoves.add(new ChessMove(position, test_move, null));
+                } else if (board.getPiece(test_move).getTeamColor() != my_color) {
+                    validMoves.add(new ChessMove(position, test_move, null));
+                    break;
+                } else { /* Is my_color. Don't add and stop in this direction */
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+        return validMoves;
+    }
 }
