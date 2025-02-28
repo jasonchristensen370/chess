44c7dd9d286b46c43073684044b49062fca2b5b6
jasonchristensen370 <jasonchristensen370@gmail.com>
2025-02-28 15:35:55 -0700

    Fixed gameID to always be non-zero.

--- server/src/main/java/dataaccess/MemoryGameDAO.java
+++ server/src/main/java/dataaccess/MemoryGameDAO.java
@@ -6,25 +6,26 @@ import model.GameData;
 import java.util.ArrayList;
 
 public class MemoryGameDAO implements GameDAO {
-
+    private final int gameIDOffset;
     private final ArrayList<GameData> database;
 
     public MemoryGameDAO() {
         database = new ArrayList<>();
+        gameIDOffset = 1;
     }
 
     public void clearGame() {
         database.clear();
     }
     public GameData createGame(String gameName) {
-        int gameID = database.size();
+        int gameID = database.size()+gameIDOffset;
         ChessGame game = new ChessGame();
         GameData newGameData = new GameData(gameID, "", "", gameName, game);
         database.add(newGameData);
         return newGameData;
     }
     public GameData getGame(int gameID) {
-        return database.get(gameID);
+        return database.get(gameID-gameIDOffset);
     }
     public ArrayList<GameData> listGames() {
         ArrayList<GameData> newList = new ArrayList<>();
@@ -34,14 +35,14 @@ public class MemoryGameDAO implements GameDAO {
         return newList;
     }
     public void updateGame(String playerColor, int gameID, String username) {
-        GameData gameInfo = database.get(gameID);
+        GameData gameInfo = database.get(gameID-gameIDOffset);
         String whiteUsername = gameInfo.whiteUsername();
         String blackUsername = gameInfo.blackUsername();
         String gameName = gameInfo.gameName();
         if (playerColor.equalsIgnoreCase("BLACK")) {
-            database.set(gameID, new GameData(gameID, whiteUsername, username, gameName, gameInfo.game()));
+            database.set(gameID-gameIDOffset, new GameData(gameID, whiteUsername, username, gameName, gameInfo.game()));
         } else {
-            database.set(gameID, new GameData(gameID, username, blackUsername, gameName, gameInfo.game()));
+            database.set(gameID-gameIDOffset, new GameData(gameID, username, blackUsername, gameName, gameInfo.game()));
         }
     }
 }
