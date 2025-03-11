e0a8f6f30ada587a971fc1b903583343129f7963
jasonchristensen370 <jasonchristensen370@gmail.com>
2025-03-11 17:33:46 -0600

    Added createAuth method.

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -3,8 +3,8 @@ package dataaccess;
 import model.AuthData;
 
 public interface AuthDAO {
-    void clearAuth();
-    AuthData createAuth(String username);
-    AuthData getAuth(String authToken);
-    void deleteAuth(String authToken);
+    void clearAuth() throws DataAccessException;
+    AuthData createAuth(String username) throws DataAccessException;
+    AuthData getAuth(String authToken) throws DataAccessException;
+    void deleteAuth(String authToken) throws DataAccessException;
 }
--- server/src/main/java/dataaccess/SQLAuthDAO.java
+++ server/src/main/java/dataaccess/SQLAuthDAO.java
@@ -3,6 +3,7 @@ package dataaccess;
 import model.AuthData;
 
 import java.sql.SQLException;
+import java.util.UUID;
 
 public class SQLAuthDAO extends SQLDAO implements AuthDAO {
     public void clearAuth() throws DataAccessException {
@@ -17,7 +18,23 @@ public class SQLAuthDAO extends SQLDAO implements AuthDAO {
         }
     }
     public AuthData createAuth(String username) throws DataAccessException {
-        return new AuthData(null, null);
+        // Create the token
+        String authToken = UUID.randomUUID().toString();
+        AuthData authData = new AuthData(authToken, username);
+
+        // Add it to the database
+        String insertStatement = "INSERT INTO authData VALUES (?, ?)";
+        try {
+            try (var conn = DatabaseManager.getConnection();
+                 var preparedStatement = conn.prepareStatement(insertStatement)) {
+                preparedStatement.setString(1, authToken);
+                preparedStatement.setString(2, username);
+                preparedStatement.executeUpdate();
+                return authData;
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
     }
     public AuthData getAuth(String authToken) throws DataAccessException {
         return new AuthData(null, null);
