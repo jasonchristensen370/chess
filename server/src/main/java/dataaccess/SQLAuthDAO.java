a162a06bed9ae9425992ad9ed303bf2e10dba9c2
jasonchristensen370 <jasonchristensen370@gmail.com>
2025-03-11 17:29:11 -0600

    Added clearAuth method.

--- server/src/main/java/dataaccess/SQLAuthDAO.java
+++ server/src/main/java/dataaccess/SQLAuthDAO.java
@@ -2,17 +2,27 @@ package dataaccess;
 
 import model.AuthData;
 
-public class SQLAuthDAO extends SQLDAO implements AuthDAO {
-    public void clearAuth() {
+import java.sql.SQLException;
 
+public class SQLAuthDAO extends SQLDAO implements AuthDAO {
+    public void clearAuth() throws DataAccessException {
+        var statement = "DELETE FROM authData";
+        try {
+            try (var conn = DatabaseManager.getConnection();
+                 var preparedStatement = conn.prepareStatement(statement)) {
+                preparedStatement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
     }
-    public AuthData createAuth(String username) {
+    public AuthData createAuth(String username) throws DataAccessException {
         return new AuthData(null, null);
     }
-    public AuthData getAuth(String authToken) {
+    public AuthData getAuth(String authToken) throws DataAccessException {
         return new AuthData(null, null);
     }
-    public void deleteAuth(String authToken) {
+    public void deleteAuth(String authToken) throws DataAccessException {
 
     }
 }
