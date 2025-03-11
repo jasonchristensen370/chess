777005ace850c6f7cccdd410d861ec82fc92fd99
jasonchristensen370 <jasonchristensen370@gmail.com>
2025-03-11 17:14:26 -0600

    Added tests for createUser.

--- server/src/test/java/dataaccess/SQLUserDAOTests.java
+++ server/src/test/java/dataaccess/SQLUserDAOTests.java
@@ -1,5 +1,6 @@
 package dataaccess;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,6 +14,16 @@ public class SQLUserDAOTests {
             DatabaseManager.createDatabase();
         } catch (DataAccessException e) {
             System.out.println(e.getMessage());
+        }
+    }
+
+    @AfterEach
+    @BeforeEach
+    public void clearTable() {
+        try {
+            new SQLUserDAO().clearUser();
+        } catch (DataAccessException _) {
+
         }
     }
 
@@ -57,11 +68,26 @@ public class SQLUserDAOTests {
 
     @Test
     public void createUserSuccess() {
-
+        try {
+            var myDAO = new SQLUserDAO();
+            myDAO.clearUser();
+            var newUser = new UserData("bobby", "passwd","email@email.com");
+            var actual = myDAO.createUser(newUser);
+            assertEquals(newUser, actual);
+        } catch (DataAccessException e) {
+            fail(e.getMessage());
+        }
     }
 
     @Test
     public void createUserFail() {
-
+        try {
+            var myDAO = new SQLUserDAO();
+            myDAO.clearUser();
+            var newUser = new UserData("bobby", null,"email@email.com");
+            assertThrows(DataAccessException.class, () -> myDAO.createUser(newUser));
+        } catch (DataAccessException e) {
+            fail(e.getMessage());
+        }
     }
 }
