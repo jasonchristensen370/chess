9b8752cc05c0c4cae5b6d09e1c0070c85cbd2f75
jasonchristensen370 <jasonchristensen370@gmail.com>
2025-03-11 21:11:52 -0600

    Added createGame method.

--- server/src/main/java/dataaccess/SQLGameDAO.java
+++ server/src/main/java/dataaccess/SQLGameDAO.java
@@ -1,8 +1,11 @@
 package dataaccess;
 
+import chess.ChessGame;
+import com.google.gson.Gson;
 import model.GameData;
 
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 public class SQLGameDAO implements GameDAO{
@@ -18,14 +21,37 @@ public class SQLGameDAO implements GameDAO{
         }
     }
     public GameData createGame(String gameName)  throws DataAccessException{
-        return new GameData(1, null, null, null, null);
+        try {
+            var serializer = new Gson();
+            ChessGame game = new ChessGame();
+            String jsonGameString = serializer.toJson(game);
+
+            String statement = "INSERT INTO gameData (gameName, game) VALUES (?, ?)";
+            try(var conn = DatabaseManager.getConnection();
+                var prepStatement = conn.prepareStatement(statement, Statement.RETURN_GENERATED_KEYS)) {
+                prepStatement.setString(1, gameName);
+                prepStatement.setString(2, jsonGameString);
+                prepStatement.executeUpdate();
+                var resultSet = prepStatement.getGeneratedKeys();
+                int gameID = 0;
+                if (resultSet.next()) {
+                    gameID = resultSet.getInt("gameID");
+                }
+                return new GameData(gameID, null, null, gameName, game);
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
     }
+
     public GameData getGame(int gameID)  throws DataAccessException{
         return new GameData(1, null, null, null, null);
     }
+
     public ArrayList<GameData> listGames()  throws DataAccessException{
         return new ArrayList<>();
     }
+
     public void updateGame(String playerColor, int gameID, String username)  throws DataAccessException{
 
     }
