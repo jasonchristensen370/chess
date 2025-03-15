7e15c61a7e405c00ddd622501a763e0162b7e650
jasonchristensen370 <jasonchristensen370@gmail.com>
2025-03-14 22:18:05 -0600

    Added some postlogin menu functionality.

--- client/src/main/java/ui/Client.java
+++ client/src/main/java/ui/Client.java
@@ -12,6 +12,7 @@ public class Client {
     private static final String BG = SET_BG_COLOR_BLACK;
     private boolean loggedIn;
     private boolean exit;
+    PrintStream out;
 
     ClientCommunicator clientCom;
 
@@ -19,59 +20,112 @@ public class Client {
         loggedIn = false;
         clientCom = new ClientCommunicator();
         exit = false;
+        out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
     }
 
     public void run() {
-        var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
-        displayWelcome(out);
-        preLoginMenu(out);
+        displayWelcome();
+        preLoginMenu();
         if (exit) {
             out.print("Bye!");
-            return;
         }
-
     }
 
-    private void displayWelcome(PrintStream out) {
+    private void displayWelcome() {
 //        out.print(BG);
-        out.println("Welcome to 240 chess. Register or login to get started.\n");
+        out.println("Welcome to 240 chess. Register or login to get started.");
     }
 
-    private void preLoginMenu(PrintStream out) {
-        while (!loggedIn) {
-            out.println("1. Register\n2. Login\n3. Quit\n4. Help\n");
-            out.print("[LOGGED OUT] >>> ");
+    private void preLoginMenu() {
+        while (!loggedIn && !exit) {
+            out.println("\n1. Register\n2. Login\n3. Quit\n4. Help");
+            out.print("\n[LOGGED OUT] >>> ");
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
             if (!isValidInput(input, 4)) {
-                out.println("Please input valid menu option number");
+                out.println("\nPlease input valid menu option number");
                 continue;
             }
-            switch(input) {
-                case "1":
-                    loggedIn = clientCom.register();
-                    if (!loggedIn) {
-                        out.println("\nFailed to login, please try again:\n");
-                    }
-                    break;
-                case "2":
-                    loggedIn = clientCom.login();
-                    if (!loggedIn) {
-                        out.println("Failed to login, please try again:");
-                    }
-                    break;
-                case "3":
-                    exit=true;
-                    break;
-                case "4":
-                    out.println("Enter one of the option numbers to get started.\n");
-            }
-            if (exit) {
-                break;
-            }
-
+            evalPreLogin(input);
+        }
+        if (!exit) {
+            postLoginMenu();
         }
     }
+
+    private void evalPreLogin(String input) {
+        switch(input) {
+            case "1":
+                loggedIn = clientCom.register();
+                if (!loggedIn) {
+                    out.println("\nFailed to login, please try again:");
+                }
+                break;
+            case "2":
+                loggedIn = clientCom.login();
+                // TODO: Remove after implementing server calls
+                loggedIn = true;
+                if (!loggedIn) {
+                    out.println("\nFailed to login, please try again:");
+                }
+                break;
+            case "3":
+                exit=true;
+                break;
+            case "4":
+                out.println("\nEnter one of the option numbers to get started.");
+        }
+    }
+
+    private void postLoginMenu() {
+        while (loggedIn) {
+            out.println("\n1. Help\n2. Logout\n3. Create Game\n4. List Games\n5. Play Game\n6. Observe Game");
+            out.print("\n[LOGGED IN] >>> ");
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            if (!isValidInput(input, 6)) {
+                out.println("\nPlease input valid menu option number");
+                continue;
+            }
+            evalPostLogin(input);
+        }
+        preLoginMenu();
+    }
+
+    private void evalPostLogin(String input) {
+        switch(input) {
+            case "1": // Help
+                out.println("\nEnter one of the option numbers to get started.");
+            case "2": // Logout
+                loggedIn = !clientCom.logout();
+                // TODO: Remove after implementing server calls
+                loggedIn = false;
+                if (loggedIn) {
+                    out.println("\nFailed to logout.");
+                }
+                break;
+            case "3": // Create Game
+                if (clientCom.createGame()) {
+                    out.println("\nGame Created!");
+                } else {
+                    out.println("\nGame was not created.");
+                }
+                break;
+            case "4": // List Games
+                if (!clientCom.listGames()) {
+                    out.println("Failed to list games.");
+                }
+                break;
+            case "5": // Play Game
+                clientCom.playGame();
+                break;
+            case "6": // Observe Game
+                clientCom.observeGame();
+                break;
+        }
+    }
+
+
 
     private boolean isValidInput(String input, int max) {
         if (!isNumeric(input)) {
